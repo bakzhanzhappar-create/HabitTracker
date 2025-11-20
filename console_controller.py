4d849476225079e95387d1cda4cca79f206c6840
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2025-11-20 17:38:25 +0500

    added console_controller

--- console_controller.py
+++ console_controller.py
@@ -1,5 +1,6 @@
 from entities import Habit
 
+
 class ConsoleController:
     __MIN_MINUTES: int = 0
     __MAX_MINUTES: int = 500
@@ -9,6 +10,26 @@ class ConsoleController:
     def __init__(self) -> None:
         ...
 
+    def request_id(self) -> int:
+        while True:
+            habit_id: str = input("Enter ID of the habit you want to delete: ").strip()
+            if self._validate_id(habit_id):
+                return int(habit_id)
+
+            else:
+                print("Invalid ID")
+
+    def request_command(self) -> str:
+        while True:
+            command: str = input(
+                "\nType 'check' to view, 'delete' to remove, 'add' to add more, or 'exit' to quit: ").strip().lower()
+
+            if self._validate_command(command):
+                return command
+
+            else:
+                print("\nInvalid command. Please try again.")
+
     def request_habit(self) -> Habit | None:
         while True:
             print("====APPENDING====")
@@ -17,13 +38,11 @@ class ConsoleController:
 
             if not self._validate_minutes(minutes):
                 print("Invalid minutes!")
-                break
 
             weight: int = int(input("Rate from 1 to 10, how important?"))
 
             if not self._validate_weight(weight):
                 print("Invalid weight!")
-                break
 
             return Habit(
                 name=habit_name,
@@ -36,3 +55,10 @@ class ConsoleController:
 
     def _validate_weight(self, weight: int) -> bool:
         return self.__LOWEST_WEIGHT <= weight <= self.__HIGHEST_WEIGHT
+
+    def _validate_command(self, command: str) -> bool:
+        available_commands: list[str] = ["add", "delete", "check", "exit"]
+        return command in available_commands
+
+    def _validate_id(self, habit_id: str) -> bool:
+        return habit_id.isdigit()
